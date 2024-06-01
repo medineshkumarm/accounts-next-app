3dc981937e035e4fb3177375c4bacc6d43a02712
medineshkumarm <medineshkumarm@gmail.com>
2024-06-01 11:59:08 +0530

    solved prisma error 2

--- db/index.ts
+++ db/index.ts
@@ -1,9 +1,18 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 
 declare global {
-    var prisma : PrismaClient | undefined;
+  var cachedPrisma: PrismaClient
 }
 
-export const db = globalThis.prisma || new PrismaClient();
+let prisma: PrismaClient
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient()
+} else {
+  if (!global.cachedPrisma) {
+    global.cachedPrisma = new PrismaClient()
+  }
 
-if(process.env.NODE_ENV !== "production") globalThis.prisma = db;
+  prisma = global.cachedPrisma
+}
+
+export const db = prisma
